92b74f610d97d5a04d5b3cafb1c34a0b1d73475d
Scott Isitt <isitts@hotmail.com>
2022-04-26 20:44:28 -0700

    Add Coin sound effect.

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip coinSFX;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(coinSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }   
     }
