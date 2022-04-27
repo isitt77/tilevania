942b22fcea41a42c81610e153d1504644c753d37
Scott Isitt <isitts@hotmail.com>
2022-04-27 01:09:44 -0700

    Preliminary setup of Player's lives text and Score text UI in TextMeshPro canvas.

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int numPlayerLives;
+    [SerializeField] TextMeshProUGUI playerLivesText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     void Awake()
     {
